69e22a745770626ba36bf11e231dbd74914deb06
Andrey Nikonov <andrey.nikonovn@gmail.com>
2020-07-30 13:22:45 +0300

    Feature. Add test method

--- app/src/main/java/jatin/demoapps/com/exoplayer2udpdemo/MainActivity.java
+++ app/src/main/java/jatin/demoapps/com/exoplayer2udpdemo/MainActivity.java
@@ -9,8 +9,12 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.extractor.Extractor;
 import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.extractor.ts.DefaultTsPayloadReaderFactory;
@@ -23,10 +27,15 @@ import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultAllocator;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.upstream.UdpDataSource;
+import com.google.android.exoplayer2.util.EventLogger;
 import com.google.android.exoplayer2.util.TimestampAdjuster;
+import com.google.android.exoplayer2.util.Util;
 
 import static com.google.android.exoplayer2.extractor.ts.TsExtractor.MODE_SINGLE_PMT;
 
@@ -44,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
         playerView = findViewById(R.id.player_view);
         playVideoBtn = findViewById(R.id.playVideo);
         videoURL = findViewById(R.id.url);
-        playVideoBtn.setOnClickListener(v -> initPlayer(videoURL.getText().toString()));
+       // playVideoBtn.setOnClickListener(v -> initPlayer(videoURL.getText().toString()));
+        playVideoBtn.setOnClickListener(v -> test(videoURL.getText().toString()));
     }
 
     private void initPlayer(String url){
@@ -61,16 +71,66 @@ public class MainActivity extends AppCompatActivity {
         // set player in playerView
         playerView.setPlayer(player);
         playerView.requestFocus();
+        player.addAnalyticsListener(new EventLogger(null));
 
 
         //Create default UDP Datasource
-        DataSource.Factory factory = () -> new UdpDataSource(3000, 100000);
+        DataSource.Factory factory = () -> new UdpDataSource(30000, 100000);
         ExtractorsFactory tsExtractorFactory = () -> new TsExtractor[]{new TsExtractor(MODE_SINGLE_PMT,
                 new TimestampAdjuster(0), new DefaultTsPayloadReaderFactory())};
         MediaSource mediaSource = new ExtractorMediaSource(videoUri, factory, tsExtractorFactory,null,null);
         player.prepare(mediaSource);
 
         // start play automatically when player is ready.
+        player.setPlayWhenReady(true);
+    }
+
+    private void test(String url){
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        LoadControl loadControl = new DefaultLoadControl(
+                new DefaultAllocator(true, C.DEFAULT_BUFFER_SEGMENT_SIZE));
+
+
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
+
+
+        Uri uri =
+                Uri.parse
+                        (url);
+
+        final DefaultBandwidthMeter bandwidthMeterA = new DefaultBandwidthMeter();
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, "teveolauncher"), bandwidthMeterA);
+
+        DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+        DataSource.Factory udsf = new UdpDataSource.Factory() {
+            @Override
+            public DataSource createDataSource() {
+                return new UdpDataSource( 3000, 100000);
+            }
+        };
+        ExtractorsFactory tsExtractorFactory = new ExtractorsFactory() {
+            @Override
+            public Extractor[] createExtractors() {
+                return new TsExtractor[]{new TsExtractor(MODE_SINGLE_PMT,
+                        new TimestampAdjuster(0), new DefaultTsPayloadReaderFactory())};
+            }
+        };
+
+
+
+        MediaSource videoSource = new ExtractorMediaSource
+                (uri, udsf, tsExtractorFactory, null, null);
+
+        playerView.setPlayer(player);
+        player.prepare(videoSource);
         player.setPlayWhenReady(true);
     }
 }
